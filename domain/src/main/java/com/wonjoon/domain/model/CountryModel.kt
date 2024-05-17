e9207443f6d201cbee09909669b24a522a1bcc69
Won-joon Shin <52183019+swj0704@users.noreply.github.com>
2024-05-17 22:26:57 +0900

    feat: CountryModel 삭제

--- domain/src/main/java/com/wonjoon/domain/model/CountryModel.kt
+++
@@ -1,9 +0,0 @@
-package com.wonjoon.domain.model
-
-import com.wonjoon.domain.model.enums.Currency
-
-interface CountryModel {
-    val name: String
-    val imageUrl : String
-    val currency : Currency
-}
--- domain/src/main/java/com/wonjoon/domain/usecase/CountryUseCase.kt
+++ domain/src/main/java/com/wonjoon/domain/usecase/CountryUseCase.kt
@@ -1,6 +1,5 @@
 package com.wonjoon.domain.usecase
 
-import com.wonjoon.domain.model.CountryModel
 import com.wonjoon.domain.model.enums.Country
 import com.wonjoon.domain.repository.CountryRepository
 
