8a2976358667252057bd258bf98156f7dc1478a8
Won-joon Shin <52183019+swj0704@users.noreply.github.com>
2024-07-15 22:47:02 +0900

    feat: MainView 함수 분리

--- presentation/src/main/java/com/wonjoon/presentation/MainActivity.kt
+++ presentation/src/main/java/com/wonjoon/presentation/MainActivity.kt
@@ -44,32 +44,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun MainView(viewModel: MainViewModel = hiltViewModel()) {
-    val rate : Double by viewModel.rate.observeAsState(0.0)
-    val amount : Double by viewModel.amount.observeAsState(0.0)
-    val date : String by viewModel.date.observeAsState("")
-    val send : Country by viewModel.send.observeAsState(Country.KR)
-    val receive : Country by viewModel.receive.observeAsState(Country.US)
-    val sendAmount = rememberSaveable {
-        mutableStateOf("0")
-    }
-
-    LaunchedEffect(send, receive) {
-        viewModel.convert(send.currency.name, receive.currency.name, receive.currency.base.toString(), true)
-    }
-
-    Column(modifier = Modifier.fillMaxSize()) {
-        HeaderView(send, receive)
-        RowLabelTextView("내 적용 환율", "${receive.currency.base}${receive.currency.currency} 당 ${rate}${send.currency.currency}")//1 달러 당 1355 원
-        RowLabelTextView("환율 조회 시간", date)
-        AmountLabelTextView(visibleLeftIcon = false, value = sendAmount.value, country = send) {
-            val number = getNumber(it)
-            sendAmount.value = formatNumber(number)
-            viewModel.convert(receive.currency.name, send.currency.name, number.toString(), false)
-        }
-        Spacer(modifier = Modifier.padding(16.dp))
-        AmountLabelTextView(visibleLeftIcon = true, value = formatNumber(amount), country = receive) {}
-    }
-}
